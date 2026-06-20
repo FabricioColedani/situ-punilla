bb113654c06421e913c75f74fb812a50bc454b04
Fabricio Coledani <115805314+FabricioColedani@users.noreply.github.com>
2026-06-20 01:38:14 -0300

    Estructura Lista

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { Inicio } from './pages/inicio/inicio';
+import { SobreNosotros } from './pages/sobre-nosotros/sobre-nosotros';
+import { Servicios} from './pages/servicios/servicios';
+import { Galeria } from './pages/galeria/galeria';
+import { Blog } from './pages/blog/blog';
+import { Contacto} from './pages/contacto/contacto';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Inicio }, // Ruta por defecto (Home)
+  { path: 'sobre-nosotros', component: SobreNosotros },
+  { path: 'servicios', component: Servicios },
+  { path: 'galeria', component: Galeria },
+  { path: 'blog', component: Blog },
+  { path: 'contacto', component: Contacto },
+  { path: '**', redirectTo: '', pathMatch: 'full' } // Redirige al inicio si la URL no existe
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,17 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+// 1. Importa los componentes aquí arriba
+import { Navbar } from './components/navbar/navbar';
+import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
+  standalone: true,
+  // 2. Agrégalos al arreglo de imports para que el HTML los reconozca
+  imports: [RouterOutlet, Navbar, Footer], 
+  templateUrl: './app.html', // (o './app.html' si tu archivo se llama así)
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('situ-app');
+  title = 'situ-app';
 }
