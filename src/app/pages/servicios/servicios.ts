8a6ebb5ce1dffc758e415b7cef0a66239b8e69c7
Fabricio Coledani <115805314+FabricioColedani@users.noreply.github.com>
2026-06-20 02:57:21 -0300

    Home y Servicio (sin Terminar)

--- src/app/pages/servicios/servicios.ts
+++ src/app/pages/servicios/servicios.ts
@@ -2,8 +2,25 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-servicios',
-  imports: [],
+  standalone: true,
   templateUrl: './servicios.html',
-  styleUrl: './servicios.css',
+  styleUrl: './servicios.css'
 })
-export class Servicios {}
+export class Servicios {
+  // Variable para controlar qué línea de colectivo se muestra (Por defecto Línea A)
+  lineaActiva: string = 'lineaA';
+  
+  // Variable para controlar si el usuario habilitó el mapa o si está bloqueado para scroll
+  mapaInteractivo: boolean = false;
+
+  // Función para cambiar de pestaña
+  cambiarLinea(linea: string) {
+    this.lineaActiva = linea;
+    this.mapaInteractivo = false; // Bloquea el mapa de nuevo al cambiar de pestaña
+  }
+
+  // Función para desbloquear el mapa y permitir zoom/movimiento
+  activarMapa() {
+    this.mapaInteractivo = true;
+  }
+}
